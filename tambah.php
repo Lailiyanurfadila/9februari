1dd115f2f063bb29b8eda5f0027383da0f508230
Lailiyanurfadila <109324295+Lailiyanurfadila@users.noreply.github.com>
2023-02-10 11:12:01 +0700

    Add files via upload

---
+++ tambah.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <table border="0">
+        <h1>Input Data</h1>
+        <form action="proses-tambah.php" method="POST">
+        <form>
+        <fieldset>
+ <p>
+    <label for="nama_peminjam">Nama Peminjam :</label>
+    <input type="text" name="nama_peminjam" />
+</p>
+<p>
+    <label for="alamat">Alamat :</label>
+    <input type="text" name="alamat" />
+</p>
+<p>
+    <label for="umur">Umur :</label>
+    <input type="text" name="umur" />
+</p>
+<p>
+    <label for="keperluan">Keperluan :</label>
+    <input type="text" name="keperluan" />
+</p>
+<p>
+    <label for="jaminan">Jaminan :</label>
+    <input type="text" name="jaminan" />
+</p>
+<p>
+    <label for="plat_nomor">Plat Nomor :</label>
+    <input type="text" name="plat_nomor" />
+</p>
+<p>
+    <label for="merek">Merek :</label>
+    <input type="text" name="merek" />
+</p>
+<p>
+    <label for="jenis_mobil">Jenis Mobil :</label>
+    <input type="text" name="jenis_mobil" />
+</p>
+<p>
+    <label for="tahun_mobil">Tahun Mobil :</label>
+    <input type="number" name="tahun_mobil" />
+</p>
+<p>
+    <label for="tanggal_pinjam">Tanggal Pinjam :</label>
+    <input type="date" name="tanggal_pinjam" />
+</p>
+<p>
+    <label for="tanggal_kembali">Tanggal Kembali :</label>
+    <input type="date" name="tanggal_kembali" />
+</p>
+<p>
+    <button type="submit" name="kirim_data">Kirim Data</button>
+</p>
+</body>
+</html>
